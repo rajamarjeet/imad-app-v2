c54c19721554b0720abaab89b9c6a9e38dcb4e17
rajamarjeet <rajamarjeet9877@gmail.com>
2017-03-05 20:50:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,51 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var contentone = {
+    title: 'article one : amit raj',
+    heading: 'article one',
+    date: 'mar 5, 2017',
+    content:`
+         <div>
+             this is article one.  this is article one.  this is article one. this is article one. this is article one. this is article one.
+        </div>
+        <div>
+             this is article one.  this is article one.  this is article one. this is article one. this is article one. this is article one.
+        </div>`
+}; 
+    
+ function createtemplate (data) {
+  var title = data.title;
+  var heading = data.heading;
+  var date = data.date;
+  var content = data.content;
+var htmltemplate =`
+ <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+             <h1>
+            ${heading}
+        </h1>
+        <div>
+            ${date}
+        </div>
+       ${content}
+        </div>
+        `;
+        return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createtemplate(articleone));
 });
 
 app.get('/article-two', function (req, res){
