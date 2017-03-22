f1738f07aeb9d744352113fb51d978c51c7cd3ce
rajamarjeet <rajamarjeet9877@gmail.com>
2017-03-22 20:16:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML = 'i am amit';
 
 var img = document.getElementById('madi');
 var marginLeft = 0;
-function moveright (){
+function moveRight (){
     marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
 }
