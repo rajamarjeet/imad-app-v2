b535fa5b67c3ed4844cdb6e1fbef5ec1935b41cb
rajamarjeet <rajamarjeet9877@gmail.com>
2017-03-26 00:04:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,22 @@
-console.log('Loaded!');
+// counter code
+var button = document.getElementById('counter');
 
-// move the image
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight (){
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
+button.onclick = function() {
+    // create a request
+    var request = new XMLHttpRequest();
+    // capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readystate === XMLHttpRequest.DONE) {
+             // Take some action
+        if(request.status === 200) {
+         var counter =  request.responseText;
+         var span = document.getElementById('count');
+           span.innerHTML = counter.toString();
+        }
+        }
+       
+    };
+    // Make a request
+    request.open('GET', 'http://rajamarjeet.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
