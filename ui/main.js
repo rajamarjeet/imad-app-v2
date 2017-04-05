41ea952ff74a52215e31cb789fc0d360dbacb349
rajamarjeet <rajamarjeet9877@gmail.com>
2017-04-06 00:34:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -51,7 +51,7 @@ submit.onclick = function() {
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
     // Make a request
-    request.open('GET', `http://rajamarjeet.imad.hasura-app.io/submit-name?name=` + name, true);
+    request.open('GET', 'http://rajamarjeet.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
 
